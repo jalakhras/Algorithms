9567834b0925b30ab1c78888d71768e9090f2608
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-01-26 22:21:35 +0200

    .

--- Algorithms/SortAlgorithm.cs
+++ Algorithms/SortAlgorithm.cs
@@ -9,11 +9,10 @@ namespace Algorithms
         //Big-o O(o^2)
         public static void  BubbleSort(int[] Numbers)
         {
-            int n = Numbers.Length;
             int temp = 0; 
-            for (int i = 0; i< n; i++) // n-Time
+            for (int i = 0; i< Numbers.Length; i++) // n-Time
             {
-                for (int j =1; j<(n-i); j++)
+                for (int j =1; j<(Numbers.Length - i); j++)
                 {
                     if(Numbers[j-1] >Numbers[j]) //n Time
                     {
